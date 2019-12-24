58e36e3d390a9dd76df90e990d93d35876ced585
lidm <lidm@cloudream.com>
2019-12-24 15:19:00 +0800

    test: 💍 create init

---
+++ example/test.ts
@@ -0,0 +1,2 @@
+const a = require("../dist/main")
+console.log(a)
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export const arrayConcat = (arr: any[], ...args: any[]) => [].concat(arr, ...args)
+
+export default {
+  arrayConcat: arrayConcat
+}
